2acae1724f4d21c13630006e4c5fabd65a979dd2
Rayzen7 <nugrahasetio6@gmail.com>
2025-07-05 02:41:48 +0700

    update

--- js/main.js
+++ js/main.js
@@ -4,7 +4,10 @@ const stars = '../assets/image/pic/testimoni/stars.png';
 const comment = '../assets/image/pic/testimoni/comment.png';
 const wrapper1 = document.getElementById('scrollWrapper1');
 const testimoniPage1 = document.getElementById('testimoniId1');
+const wrapper2 = document.getElementById('scrollWrapper2');
+const testimoniPage2 = document.getElementById('testimoniId2');
 let testimoniPage1Html = '';
+let testimoniPage2Html = '';
 let scrollSpeed = 1;
 
 testimoniData1.forEach((data) => {
@@ -36,10 +39,39 @@ wrapper1.addEventListener('scroll', () => {
   }
 });
 
+testimoniData2.forEach((data) => {
+    testimoniPage2Html += `
+        <div class="flex flex-col gap-6 rounded-lg p-6  w-[400px] border-primary">
+            <div class="flex justify-between items-center">
+              <div class="flex justify-center items-center gap-4">
+                <img src="${data.image}" class="w-[70px] h-auto rounded-full" alt="">
+                <div class="flex flex-col gap-2">
+                  <h1 class="text-black font-semibold text-[20px]">${data.name}</h1>
+                  <img src="${stars}" class="w-[100px] h-auto" alt="">
+                </div>
+              </div>
+              <img src="${comment}" class="w-[40px] h-auto" alt="">
+            </div>
+            <div class="w-full">
+              <p class="font-medium text-gray text-[14px] text-justify">${data.text}</p>
+            </div>
+        </div>
+    `
+});
+
+testimoniPage2.innerHTML += testimoniPage2Html;
+wrapper2.scrollLeft = testimoniPage2.scrollWidth / 2;
+
 function autoScroll() {
     wrapper1.scrollLeft += scrollSpeed;
+    wrapper2.scrollLeft -= scrollSpeed;
+
     if (wrapper1.scrollLeft >= testimoniPage1.scrollWidth / 2) {
         wrapper1.scrollLeft = 0;
+    }
+
+    if (wrapper2.scrollLeft <= 0) {
+        wrapper2.scrollLeft = testimoniPage2.scrollWidth / 2;
     }
 
     requestAnimationFrame(autoScroll);
