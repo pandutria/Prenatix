0a315a9472b0b594cc1605075916394c40ad9ca2
pandutria <panduadyatama029@gmail.com>
2025-07-13 23:35:17 +0700

    cdsnjsc

---
+++ js/data/consultation.js
@@ -0,0 +1,14 @@
+const img1 = "../../assets/image/pic/consultation/1.png"
+
+const consultation = [
+    {
+        id: 1,
+        name: "Syifa Maulida",
+        category: "Dokter Kandungan",
+        image: img1,
+        price: 50000,
+        specialization: ["Pemeriksa Kehamilan", "USG", "Persalinan"]
+    },
+]
+
+export default consultation;
